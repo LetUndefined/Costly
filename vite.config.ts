db8f4898cc99d4ee983dbc8020e345059bbb3670
Sandro <sandroodangelo@hotmail.com>
2026-04-21 16:45:52 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/Pricey/' : '/',
+  base: command === 'build' ? '/Costly/' : '/',
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
